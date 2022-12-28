751644280db2ebb8d63c687df45689ad523ea728
Yoly Keys <116820221+codewithkeys@users.noreply.github.com>
2022-12-28 04:34:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+is_magician = False
+is_expert = False
+
+if is_magician and is_expert:
+    print("You are a master magician.")
+elif is_magician:
+    print("At least you're getting there.")
+elif is_expert:
+    print("You need magic powers.")
+else :
+    print("You ain't shit!")
+    
